a42d94b998d4902981d0a7065b8e60ea86545e1d
Amanpreet325 <amanpreetsingh2019463@gmail.com>
2024-03-29 15:37:28 +0530

    initial commit

---
+++ routes/post.js
@@ -0,0 +1,37 @@
+// Import Mongoose
+const mongoose = require('mongoose');
+
+// Define the Post Schema
+const postSchema = new mongoose.Schema({
+  caption: {
+    type: String,
+    required: true,
+  },
+  image:{
+    type:String,
+  },
+  createdAt: {
+    type: Date,
+    default:Date.now,
+  },
+  description:{
+    type: String,
+    
+  },
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'user',
+  },
+  likes:{
+    type:Array,
+    default:[],
+  },
+  
+  
+});
+
+// Create the Post model
+
+
+// Export the Post model
+module.exports=mongoose.model('post', postSchema);
